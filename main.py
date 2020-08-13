7a0f1b5ce57692d566b0e4bda7ea60f0f0ace222
Maciej Ledwoch <1412315+mymain@users.noreply.github.com>
2020-08-13 19:32:43 +0200

    try{}catch{} end of comments list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 
@@ -9,7 +10,7 @@ def run():
     chrome driver download > https://sites.google.com/a/chromium.org/chromedriver/downloads
     """
     # YT video url to scrape comments
-    url = 'https://www.youtube.com/watch?v=sMIMn9_nFpo'
+    url = 'https://www.youtube.com/watch?v=psjtvrQJ20I'
 
     print('Pulling comments list from: ' + url)
 
@@ -36,17 +37,24 @@ def run():
         # TODO change for loader ready state
         time.sleep(10)  # not nice but working
 
-        for x in range(100):
-            continuations_section = driver\
-                .find_element_by_css_selector('#continuations .ytd-item-section-renderer:last-child')
-            driver.execute_script('arguments[0].scrollIntoView();', continuations_section)
+        while True:
+            try:
+                comments_section_end = driver\
+                    .find_element_by_css_selector('#continuations .ytd-item-section-renderer:last-child')
+                driver.execute_script('arguments[0].scrollIntoView();', comments_section_end)
 
-            time.sleep(5)
-            # detect end of comments?
+                time.sleep(5)
+                # detect end of comments?
+
+            except NoSuchElementException:
+                print('Comments end')
+                break
+
+        print('Ok we are ready - all comments are loaded')
 
         # get comments contents these are arrays
         users = driver.find_elements_by_id('author-text')
-        comments = driver.find_elements_by_id('content-text')
+        comments = driver.find_elemefints_by_id('content-text')
 
         for user, comment in zip(users, comments):
             print(user.text + ':')
